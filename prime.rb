00e2f80fde64d29902ae60c3c35aebe11388011b
Jean Gérard Bousiquot <jgb@noukod.com>
2018-09-20 15:02:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime? number
   # (((2..number).to_a).any? {|n| number % n == 0 && number > 1})
   values = []
 
-  (2...number).to_a.each { |n| number % n !=0 ? values << true : values << false }
+  (2...number).to_a.each { |n| number % n != 0 ? values << true : values << false }
 
   !values.include?(false) && number > 1 ? true : false
 end
