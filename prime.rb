bb3005c1538671722ea1e12563194f77e806f330
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-09 19:16:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(prime_number)
+  begin_number = 2
+    if prime_number > 1
+        interval = (begin_number..prime_number-1).to_a
+        interval.none? do |container_number| 
+        prime_number % container_number == 0
+    end
+    else
+        false
+    end
+end
