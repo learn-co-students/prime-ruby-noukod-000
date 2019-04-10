c947a5e360d82c580a42c853bd3e42dbeba3591c
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-10 02:26:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  start_number = 2
+  if number > 1 
+    range = (start_number..number-1).to_a
+    range.none? do |array| 
+      number % array == 0
+    end
+  else
+    false
+  end
+end
+ prime?(2)
