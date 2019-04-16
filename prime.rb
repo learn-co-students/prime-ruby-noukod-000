aefcd1d2ad244677bd9ad0f94d74b2680e6dc165
James Declerus <james.declerus@esih.edu>
2019-04-16 18:27:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime? (n)
-    if n <= 1
-        false
-    elsif n == 2
-        true
+def prime? (input_number)
+    if input_number <= 1
+      return  false
+    elsif input_number == 2
+    return  true
     else
-        (2..n/2).none? { |i| n % i == 0}
+        (2..input_number/2).none? { |i| input_number % i == 0}
     end
 end
