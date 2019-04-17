54142f971d6a88cfa0a675fa3fcf5b48e1e0450b
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 18:56:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(number)
+  prime = true
+  if number == 1 || number < 1
+    prime = false
+  end
+  count = 2
+
+    while count < number
+          if number % count == 0
+            prime =  false
+          end
+          count += 1
+    end
+    return prime
+  end
